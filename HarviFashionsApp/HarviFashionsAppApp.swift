53183179d51204201c8547b41c546b844392ed2b
Lakshmi Reddy Motati <reddy.lakshmi@gmail.com>
2026-04-16 22:34:42 -0500

    Initial Commit

---
+++ HarviFashionsApp/HarviFashionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarviFashionsAppApp.swift
+//  HarviFashionsApp
+//
+//  Created by Lakshmi Reddy Motati on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct HarviFashionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
